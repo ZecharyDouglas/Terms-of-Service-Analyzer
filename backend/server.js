69bbb01a769f9caaf974f737c81730b55e55b50a
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2026-04-24 22:31:36 -0400

    new keys + successful use of bedrock class

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 const OpenAI = require("openai");
 const app = express();
 port = 3000;
 //pass parameters to the function
-const client = OpenAI();
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  //baseURL: process.env.OPENAI_BASE_URL,
+});
 
 app.get("/", (req, res) => {
   res.send("Server alive");
@@ -12,17 +15,22 @@ app.get("/", (req, res) => {
 //post request to send the webpage content to the backend
 app.post("/posttext", (req, res) => {});
 //get request to push data back to the front end
-app.get("/getresponse", async () => {
+app.get("/getresponse", async (req, res) => {
   const response = await client.responses.create({
     model: "openai.gpt-oss-120b",
     input: [
       {
         role: "user",
-        content: "Write a five sentence bedtime story about a cow.",
+        content:
+          "Analyze this web page and tell me if there is any potentially legally harmful content in it.",
       },
     ],
   });
-  console.log(response.output_text);
+
+  const message = response?.output_text;
+  return res.json({
+    message: message,
+  });
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
